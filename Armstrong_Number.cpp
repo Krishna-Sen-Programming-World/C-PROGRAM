ab79e8d61ff26a5127759248f36223a7365e1015
Krishna Sen <98060735+Krishna-Sen-Programming-World@users.noreply.github.com>
2022-07-04 01:27:58 +0530

    Update Armstrong_Number.cpp

--- Armstrong_Number.cpp
+++ Armstrong_Number.cpp
@@ -2,23 +2,19 @@
 #include<math.h>
 int main()
 {
-     int num,dup,sum=0,dig=0,r;
+    int num,size=0,sum=0;
     printf("enter the number:");
     scanf("%d",&num);
-    dup=num;
-     while(dup>0)
-     {dup=dup/10;
-      dig++;
-      }
-      dup=num;
-      while(dup>0)
-      {   r=dup%10;
-          sum=sum+pow(r,dig);
-           dup=dup/10;
-      }
-      if(num==sum)
-       printf("the number is ARMSTRONG");
+    for(int i=num;i>0;i/=10)
+    size++;
+    for(int i=num;i>0;i/=10)
+    {
+        int r=i%10;
+        sum=sum+pow(r,size);
+     }
+     if(num==sum)
+     printf("the number is ARMSTRONG");
      else
-        printf("the number is NOT ARMSTRONG");
+     printf("the number is NOT ARMSTRONG");
 return 0;
 }
